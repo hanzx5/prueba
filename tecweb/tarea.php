1e84e12f8864e64c1cefd43301d3d807d7a9d164
hanzx5 <2721681891noe@gmail.com>
2022-09-19 23:06:55 -0500

    commit

---
+++ tecweb/tarea.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Document</title>
+</head>
+<body>
+  
+<?php   
+/*******************1*********************/
+$_myvar = 1;
+  $myvar = 2;
+ $_7var = 3;
+$_element1 = 4;
+    echo "*******************1*********************<br><br>";
+    echo "las variables _myvar,  myvar, _7var y _element1 son validas \n";
+    echo "las demas no son validas debido a que no se puede tener un numero o simbolo en las variables y
+    en el caso de las variables que empieza con _ es valida porque empieza con un caracter de subrayado";
+    echo "<br>";
+    echo "$_myvar,$myvar,$_7var,$_element1 <br>";
+    /*******************2*********************/
+
+?>
+
+
+</body>
+</html>
